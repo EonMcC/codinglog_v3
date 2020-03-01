a119dc9238117558831555a63b5cb0b2721791e5
Iain <iain2eyes@gmail.com>
2020-03-01 13:28:01 +0000

    sql_runner changed

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,7 +5,7 @@ class SqlRunner
   def self.run( sql, values = [] )
     begin
       db = PG.connect({ dbname: 'd67640eqidsj14', host: 'ec2-35-172-85-250.compute-1.amazonaws.com
-      ', port: '5432', user: 'vfydmdhpqfprps', password: '61a633ebc9be273196de854c914faf8161a4809f17bab6109c400d81daff6e6c' })
+      ', port: 5432, user: 'vfydmdhpqfprps', password: '61a633ebc9be273196de854c914faf8161a4809f17bab6109c400d81daff6e6c' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
       # uncomment below for local running.
@@ -13,7 +13,7 @@ class SqlRunner
       # db.prepare("query", sql)
       # result = db.exec_prepared( "query", values )
     ensure
-      db.close() if db != nil
+      # db.close() if db != nil
     end
     return result
   end

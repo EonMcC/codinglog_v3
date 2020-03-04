877d5f219367f83cce6f828bea5633ab10af1d84
Iain <iain2eyes@gmail.com>
2020-03-04 19:47:36 +0000

    Revert "change port to string"
    This reverts commit 46ff6a51937f963fb35f8d5b651248d9e8a2bb1f.

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,8 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'd67640eqidsj14', host: 'ec2-35-172-85-250.compute-1.amazonaws.com
-      ', port: '5432', user: 'vfydmdhpqfprps', password: '61a633ebc9be273196de854c914faf8161a4809f17bab6109c400d81daff6e6c' })
+      db = PG.connect({ dbname: 'd67640eqidsj14', host: 'postgres://vfydmdhpqfprps:61a633ebc9be273196de854c914faf8161a4809f17bab6109c400d81daff6e6c@ec2-35-172-85-250.compute-1.amazonaws.com:5432/d67640eqidsj14', port: 5432, user: 'vfydmdhpqfprps', password: '61a633ebc9be273196de854c914faf8161a4809f17bab6109c400d81daff6e6c' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
       # uncomment below for local running.
